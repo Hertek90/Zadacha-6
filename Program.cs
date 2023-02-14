0ec19f92930d5031a06f7a3eab3c240f371061cc
«Apolon» <hertek1990@gmail.com>
2023-02-14 22:36:14 +0700

    Zadacha 6

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = number % 2;
+if (x > 0)
+{
+    System.Console.WriteLine("Нечетное");
+}
+else 
+{
+    System.Console.WriteLine("Четное");
+}
